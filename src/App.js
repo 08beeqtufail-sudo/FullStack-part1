d253f959618c9c0e70cad0d7f6a588559b8e4024
08beeqtufail-sudo <08beeqtufail@gmail.com>
2021-04-05 17:31:47 +0200

    Commit for exercise 1.5

--- src/App.js
+++ src/App.js
@@ -47,31 +47,36 @@ const Content = (props) => {
     }
     
 const App= () => {
-  const course = 'Half Stack application development'
-  const parts = [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course = {
+   name:'Half Stack application development',
+   parts : [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+  
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+  
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+  
+    }
+    
+  ]
 
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
 
   }
   
-]
 
 return(
 <div>
-<Header name = {course} />
-<Content part = {parts}/>
-<Total  part= {parts} />
+<Header name = {course.name} />
+<Content part = {course.parts}/>
+<Total  part= {course.parts} />
 </div>
 
   )
