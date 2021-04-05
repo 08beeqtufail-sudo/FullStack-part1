e70707b30241058eb2e256e85b0de3969473f5fa
08beeqtufail-sudo <08beeqtufail@gmail.com>
2021-04-05 17:27:57 +0200

    Commit for exercise 1.4

--- src/App.js
+++ src/App.js
@@ -26,9 +26,9 @@ const Content = (props) => {
 
   return (
   <div>
-  <Part part = {props.part1}/>
-  <Part part = {props.part2}/>
-  <Part part = {props.part3}/>
+  <Part part = {props.part[0]}/>
+  <Part part = {props.part[1]}/>
+  <Part part = {props.part[2]}/>
   </div>
   
     )
@@ -39,7 +39,7 @@ const Content = (props) => {
     return (
     <div>
     
-    <p> Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises} </p>
+    <p> Number of exercises {props.part[0].exercises + props.part[1].exercises + props.part[2].exercises} </p>
     </div>
     
       )
@@ -48,25 +48,30 @@ const Content = (props) => {
     
 const App= () => {
   const course = 'Half Stack application development'
-  const part1 = {
-  name:'Fundamentals of React',
-  exercises:10
-  }
+  const parts = [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
 
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+
+  },
+  {
     name: 'State of a component',
     exercises: 14
+
   }
+  
+]
 
 return(
 <div>
 <Header name = {course} />
-<Content part1= {part1} part2= {part2} part3= {part3}/>
-<Total  part1= {part1} part2= {part2} part3= {part3} />
+<Content part = {parts}/>
+<Total  part= {parts} />
 </div>
 
   )
